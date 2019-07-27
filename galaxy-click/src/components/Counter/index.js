63d9fa1eb4066e1afdea34e6d97a25891f029290
Phil Dugliss <phillipdugliss@gmail.com>
2019-07-27 10:15:08 -0500

    Hide Score Updated

--- galaxy-click/src/components/Counter/index.js
+++ galaxy-click/src/components/Counter/index.js
@@ -5,7 +5,7 @@ function Counter(event){
     return(
         <div className="jumbotron jumbotron-fluid" id="counter">
         <div className="container">
-            <h1 className="display-4" id="scoreClicked" >{event.score}</h1>
+            <h1 className="display-4" id="scoreClicked" style={(event.score > 0)? {}:{display:'none'}} > {event.score} </h1>
             <h2 className="lead" id="winLose"> {event.userMessage}</h2>
         </div>
         </div>
